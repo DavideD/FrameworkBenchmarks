d71fd8adbf13a1416498564f1bd2ba18353e4905
Davide D'Alto <davide@hibernate.org>
2021-01-13 16:27:35 +0000

    Add new module for Hibernate Reactive + RestEasy + CompletionStage

---
+++ frameworks/Java/quarkus/resteasy-reactive-hibernate-reactive-stage/src/main/java/io/quarkus/benchmark/repository/BaseRepository.java
@@ -0,0 +1,20 @@
+package io.quarkus.benchmark.repository;
+
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
+import javax.inject.Inject;
+
+import org.hibernate.reactive.mutiny.Mutiny;
+import org.hibernate.reactive.stage.Stage;
+
+import io.smallrye.mutiny.Uni;
+
+public class BaseRepository {
+
+	@Inject
+	protected Stage.SessionFactory sf;
+
+	public <T> CompletionStage<T> inSession(Function<Stage.Session, CompletionStage<T>> work) {
+		return sf.withSession( session -> work.apply( session ) );
+	}
+}
