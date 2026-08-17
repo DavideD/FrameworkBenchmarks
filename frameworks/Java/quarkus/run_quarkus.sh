2a45b2d646c7365b53d6a132e64805878191eef1
Davide D'Alto <davide@hibernate.org>
2026-08-17 18:31:51 +0200

    Revert "Enable JFR profiling in run_quarkus.sh for flamegraph generation"
    This reverts commit cf083c4ba78c5d5112a8f33ac12a4b6030139a28.

--- frameworks/Java/quarkus/run_quarkus.sh
+++ frameworks/Java/quarkus/run_quarkus.sh
@@ -10,10 +10,6 @@
 # Consider using -Dquarkus.http.io-threads=$((`grep --count ^processor /proc/cpuinfo`)) \
 
 JAVA_OPTIONS="-server \
-  -XX:+FlightRecorder \
-  -XX:StartFlightRecording=delay=15s,duration=60s,filename=/quarkus/trace.jfr,settings=profile \
-  -XX:+UnlockDiagnosticVMOptions \
-  -XX:+DebugNonSafepoints \
   -Dquarkus.http.limits.max-body-size= \
   -Dquarkus.vertx.prefer-native-transport=true  \
   -XX:-StackTraceInThrowable \
